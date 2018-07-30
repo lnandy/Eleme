005633b3633e0201fc2f5905814645dcfdc61f6c
Ji Wei Li <liljwljw@cn.ibm.com>
2018-07-30 15:38:45 +0800

    update

--- src/store/modules/shop.js
+++ src/store/modules/shop.js
@@ -9,11 +9,7 @@ const state = {
 const getters = {
   getShopList (state, getters, rootState) {
     return !!state.list.items ? state.list.items.map(i=>{
-      let tempPath = i.restaurant.image_path;
       let tempDistance = i.restaurant.distance.toString();
-      i.restaurant.image_path = "http://fuss10.elemecdn.com/" + tempPath.slice(0,1) + '/' + tempPath.slice(1,3) 
-                              + '/' + tempPath.slice(3,tempPath.length)
-                              + (tempPath.includes('png') ? '.png' : '.jpeg') + '?imageMogr/format/webp/thumbnail/!130x130r/gravity/Center/crop/130x130/';
       if(tempDistance.length > 3){
         tempDistance = tempDistance.slice(0,tempDistance.length-3) + '.' + tempDistance.slice(-3);
         tempDistance = parseFloat(tempDistance).toFixed(2) + 'km';
